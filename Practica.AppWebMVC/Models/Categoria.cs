4f7ec1ff01e6d42fd5882beab30b2119a3544e5b
CHATO <colochocristian@outlook.com>
2025-03-08 22:00:02 -0600

    conectando con la base de datos

---
+++ Practica.AppWebMVC/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica.AppWebMVC.Models;
+
+public partial class Categoria
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+}
